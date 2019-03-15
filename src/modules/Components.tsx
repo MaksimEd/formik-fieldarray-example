1b6d69807c5bad4cd634f37c47e99948fd915f80
edishions <edishions@gmail.com>
2019-03-15 17:57:47 +0300

    edit tslint and edit style input autocomplite

--- src/modules/Components.tsx
+++ src/modules/Components.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import InputAutoComplete from './InputAutoComplete';
 
 const Componnents = () => (
-    <div style={{maxWidth: 1000, margin: '0 auto'}}>
+    <div style={{maxWidth: 1000, margin: '0 auto', position: 'relative'}}>
         <InputAutoComplete />
     </div>
 );
--- src/modules/InputAutoComplete/index.tsx
+++ src/modules/InputAutoComplete/index.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { TextField, MenuItem, Popper, Paper, ClickAwayListener, MenuList, Grow } from '@material-ui/core';
+import { 
+  ClickAwayListener, 
+  Grow, 
+  MenuItem, 
+  MenuList, 
+  Paper, 
+  Popper, 
+  TextField 
+} from '@material-ui/core';
 
 const options = [
   'None',
@@ -23,10 +31,11 @@ const InputAutoComplete = ({}) => {
   const [isOpenMenu, toggleMenuOpen] = React.useState(false);
   const [value, setValue] = React.useState('');
   return (
-    <div style={{position: 'relative'}}>
+    <div style={{position: 'relative', width: 200}}>
       <TextField
         label="Name"
         inputRef={refInput}
+        fullWidth
         // value={this.state.name}
         onChange={e => (setValue(e.target.value.trim().toUpperCase()), toggleMenuOpen(true))}
         margin="normal"
@@ -41,12 +50,9 @@ const InputAutoComplete = ({}) => {
             <Paper style={{width: '100%', maxHeight: 200, overflowY: 'auto'}}>
               <ClickAwayListener onClickAway={() => toggleMenuOpen(false)}>
                 <MenuList>
-                  {options.reduce(
-                    (accum: JSX.Element[], curr) => (
-                      curr.toUpperCase().includes(value) ? [...accum, <MenuItem key={curr}>{curr}</MenuItem>] : accum
-                    ),              
-                    []
-                  )}
+                  {options
+                    .filter(item => item.toUpperCase().includes(value))
+                    .map(item => <MenuItem key={item}>{item}</MenuItem>)}
                 </MenuList>
               </ClickAwayListener>
             </Paper>
